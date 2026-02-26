2b005a7ea329a38bd7a2eb0ebcba61813adf3175
dansteamer1337-lab <dansteamer1337@gmail.com>
2026-02-26 21:41:51 +0300

    Добавьте файлы проекта.

---
+++ SimpleApp.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using SimpleApp;
+
+namespace SimpleApp.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void AddNumbers_ShouldReturnCorrectSum()
+        {
+            int result = Program.AddNumbers(5, 7);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void AddNumbers_ShouldHandleNegativeNumbers()
+        {
+            int result = Program.AddNumbers(-3, 7);
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ SimpleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SimpleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Введите второе число:");
+            string input2 = Console.ReadLine();
+
+            if (int.TryParse(input1, out int number1) &&
+                int.TryParse(input2, out int number2))
+            {
+                int result = AddNumbers(number1, number2);
+                Console.WriteLine($"Результат сложения: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введите корректные числа.");
+            }
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
